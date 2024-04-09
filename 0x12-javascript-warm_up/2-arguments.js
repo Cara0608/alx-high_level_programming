297e7264822d804734a6f11f52ab4891c149aea5
Cara <Caraehizele0607@gmail.com>
2024-04-09 17:35:23 +0100

    Javascript

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length === 2) {
-	console.log("No arguments found");
-	else if (process.argv.length === 3) {
-		console.log("Argument found");
-		else {
-			console.log("Arguments found");
-		}
-	}
+console.log("No arguments found");
+}else if (process.argv.length === 3) {
+console.log("Argument found");
+}else {
+console.log("Arguments found");
 }
