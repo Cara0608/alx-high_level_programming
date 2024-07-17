78356ad354308fe498a4ce2dd1bbc132371b542f
Cara <Caraehizele0607@gmail.com>
2024-07-17 08:32:02 +0000

    Pyhton tasks

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a string-to-JSON function."""
+import json
+
+
+def to-json-string(my_obj):
+"""Return the JSON representation of a string object."""
+return json.dumps(my_obj)
