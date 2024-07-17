0e2482397f3444e91565da85e4115b6d4468e262
Cara <Caraehizele0607@gmail.com>
2024-07-17 08:36:22 +0000

    Pyhton tasks

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,13 +3,13 @@
 
 
 def append_write(filename="", text=""):
-"""Appends a string to the end of a UTF8 text file.
+    """Appends a string to the end of a UTF8 text file.
 
-Args:
-filename (str): The name of the file to append to.
-text (str): The string to append the file.
-Returns:
-The number of characters appended.
-"""
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append the file.
+    Returns:
+        The number of characters appended.
+    """
 with open(filename, "a", encoding="utf-8") as f:
-return f.write(text)
+    return f.write(text)
